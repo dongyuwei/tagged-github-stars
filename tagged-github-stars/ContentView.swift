ea265f67ffeba7c1afd81f4b2c29f31be6084a37
Dong Yuwei <newdongyuwei@gmail.com>
2020-05-28 08:54:18 +0800

    init project

---
+++ tagged-github-stars/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  tagged-github-stars
+//
+//  Created by Dong Yuwei  on 2020/5/28.
+//  Copyright © 2020 Dong Yuwei . All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("github stars")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
