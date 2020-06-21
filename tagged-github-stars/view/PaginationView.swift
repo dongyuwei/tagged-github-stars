16fa5c93d473f43310f235e2c82842d9494c2bc6
Dong Yuwei <newdongyuwei@gmail.com>
2020-06-21 16:42:58 +0800

    disable pagination Buttons if no corresponding pages

--- tagged-github-stars/view/PaginationView.swift
+++ tagged-github-stars/view/PaginationView.swift
@@ -6,37 +6,29 @@ struct PaginationView: View {
     var body: some View {
         HStack {
             Spacer()
-            if ((self.store.pagination["first"]) != nil) {
-                Button(action: {
-                    self.store.loadStars(self.store.token, userName: self.store.basicUserInfo.name, url: self.store.pagination["first"])
-                }) {
-                    Text("First")
-                }
-            }
+            Button(action: {
+                self.store.loadStars(self.store.token, userName: self.store.basicUserInfo.name, url: self.store.pagination["first"])
+            }) {
+                Text("First")
+            }.disabled(self.store.pagination["first"] == nil)
             
-            if((store.pagination["prev"]) != nil) {
-                Button(action: {
-                    self.store.loadStars(self.store.token, userName: self.store.basicUserInfo.name, url: self.store.pagination["prev"])
-                }) {
-                    Text("Prev")
-                }
-            }
+            Button(action: {
+                self.store.loadStars(self.store.token, userName: self.store.basicUserInfo.name, url: self.store.pagination["prev"])
+            }) {
+                Text("Prev")
+            }.disabled(self.store.pagination["prev"] == nil)
             
-            if((store.pagination["next"]) != nil) {
-                Button(action: {
-                    self.store.loadStars(self.store.token, userName: self.store.basicUserInfo.name, url: self.store.pagination["next"])
-                }) {
-                    Text("Next")
-                }
-            }
-
-            if((store.pagination["last"]) != nil) {
-                Button(action: {
-                    self.store.loadStars(self.store.token, userName: self.store.basicUserInfo.name, url: self.store.pagination["last"])
-                }) {
-                    Text("Last")
-                }
-            }
+            Button(action: {
+                self.store.loadStars(self.store.token, userName: self.store.basicUserInfo.name, url: self.store.pagination["next"])
+            }) {
+                Text("Next")
+            }.disabled(self.store.pagination["next"] == nil)
+            
+            Button(action: {
+                self.store.loadStars(self.store.token, userName: self.store.basicUserInfo.name, url: self.store.pagination["last"])
+            }) {
+                Text("Last")
+            }.disabled(self.store.pagination["last"] == nil)
         }.frame(width: 290)
     }
 }
